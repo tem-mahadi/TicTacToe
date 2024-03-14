64d01af1957e4b2b89042f37c5f17b8146674fff
Hasan Al Mahadi <mahadi4uruetcse21@gmail.com>
2024-03-15 05:20:39 +0600

    splashScreen

--- app/src/main/java/com/temmahadi/tictactoe/SplashScreen.java
+++ app/src/main/java/com/temmahadi/tictactoe/SplashScreen.java
@@ -2,22 +2,25 @@ package com.temmahadi.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent next = new Intent(SplashScreen.this,StartPage.class);
-
-        new Handler().postDelayed(new Runnable() {
+        Intent nex = new Intent(SplashScreen.this,StartPage.class);
+        //parallel Thread
+        new Handler().postDelayed(new Runnable() { //delay 3s before running run() method
             @Override
             public void run() {
-                startActivity(next);
+                startActivity(nex);
+                finish();
             }
         },3000);
     }
