6ed03ed8a01fcbf74a17aaddfda128a650563ee6
Hasan Al Mahadi <mahadi4uruetcse21@gmail.com>
2024-03-16 04:15:40 +0600

    fixed

--- app/src/main/java/com/temmahadi/tictactoe/SplashScreen.java
+++ app/src/main/java/com/temmahadi/tictactoe/SplashScreen.java
@@ -6,15 +6,21 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
-
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         Intent nex = new Intent(SplashScreen.this,StartPage.class);
+        Animation start= AnimationUtils.loadAnimation(this,R.anim.starting);
+        t=findViewById(R.id.start);
+        t.setAnimation(start);
         //parallel Thread
         new Handler().postDelayed(new Runnable() { //delay 3s before running run() method
             @Override
@@ -22,6 +28,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(nex);
                 finish();
             }
-        },2000);
+        },3000);
     }
 }
